c2ebc6cc52dee6e121dd60a769a6080fa8b00459
Juan Kevin Trujillo <juankevin.tr@gmail.com>
2019-10-30 09:40:59 +0000

    Initial commit!

---
+++ README.md
@@ -0,0 +1,4 @@
+# IS2-Kata4
+
+**Objetivos:**
+- Dado un conjunto de direcciones de email almacenadas en un fichero de texto mostrar el histograma de los dominios y visualizarlo.
---
+++ src/com/juankevintrujillo/model/Histogram.java
@@ -0,0 +1,27 @@
+package com.juankevintrujillo.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author juankevintr
+ */
+public class Histogram <T> {
+    
+    private final Map<T, Integer> map = (Map<T, Integer>) new HashMap<T,Integer>();
+    
+    public Integer get (T key){
+        return map.get(key);
+    }
+    
+    public Set<T> keySet(){
+        return map.keySet();
+    }
+    
+    public void increment(T key){
+        map.put(key, map.containsKey(key) ? map.get(key) + 1 : 1);
+    }
+    
+}
